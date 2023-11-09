b28b88fd1260f78795ca586242c6c174891d63f5
Vinicius Borges <vinicius.borges@ae.studio>
2023-11-09 11:08:05 -0300

    feat: update extension link

--- src/App/LandingPage.tsx
+++ src/App/LandingPage.tsx
@@ -15,7 +15,7 @@ export const LandingPage = () => {
           Try Layout Comparison today: available for FREE on the Chrome web store!
         </h2>
         <a
-          href="https://chrome.google.com/webstore"
+          href="https://chromewebstore.google.com/detail/pic-go/kaebiloefbpmnihheoceockhjdmjbhcn?hl=en"
           target="_blank"
           rel="noreferrer"
           className="flex items-center gap-2 bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded-md disabled:opacity-25 disabled:cursor-not-allowed"
